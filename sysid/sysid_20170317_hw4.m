4ec59d84a48a968d690eebdd3d9aed1d0ec63239
mickey9910326 <mickey9910326@gmail.com>
2017-03-07 23:29:25 +0800

    sysid update

---
+++ sysid/sysid_20170317_hw4.m
@@ -0,0 +1,32 @@
+clear;clc;
+A  = 2;
+PHI =
+V0 = 30;
+Lambda = 0.87;
+t  = [0:0.01:20];
+dt = 0.01;
+n  = randi([0 100],1,length(t)) *dt;
+
+Vt = A * exp( -1*Lambda*t ) + cos(W*t+PHI);
+
+plot(t,Vt)
+
+point = [1;100;20;130;120;140;150;200];
+
+for i=1:length(point)
+    Y(i,1) = log(Vt(point(i)));
+    A(i,1) = 1;
+    A(i,2) = -t(point(i));
+end
+
+theta_hat = inv(A'*A)*(A')*Y;
+e_hat = Y-A*theta_hat;
+v0 = exp(theta_hat(1))
+lambda = theta_hat(2)
+
+
+vt = v0 * exp( -1*lambda*t );
+hold on;
+plot(t,vt,'g')
+
+[theda,Yh,E,E_std,theda_std,theda_cov,E_corrcoef] = lse_std(Y,A);
